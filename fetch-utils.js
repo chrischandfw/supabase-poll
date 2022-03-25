2c0029f7ebaf7adcb9f8ec03eec09a0cbc679b08
chris_chan <chrischandfw@gmail.com>
2022-03-25 17:06:08 -0500

    added async functions for sign up page

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indra3ViZGVpanZybnRkbXVucWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc1NTM1MTcsImV4cCI6MTk2MzEyOTUxN30.qE-NCGctQQqftyEJnJ49hNdOGf4jDNo61YYMlpDAr2g';
 
 const SUPABASE_URL = 'https://wkkubdeijvrntdmunqer.supabase.co';
@@ -19,7 +20,36 @@ export async function getPolls() {
     const response = await client
         .from('poll_farm')
         .select('*');
-    console.log(response);
+
     return response.body;
 }
 
+export async function getUser() {
+    return client.auth.user();
+}
+
+export function checkLoggedIn() {
+    if (!client.auth.session()) {
+        window.location = '../';
+    }
+}
+
+export async function logout() {
+    await client.auth.signOut();
+
+    return window.location.href = '../';
+}
+
+export async function signUp(realEmail, realPassword) {
+
+    console.log('before sign up', client.auth.user());
+
+    const response = await client.auth.signUp({
+        email: realEmail,
+        password: realPassword,
+    });
+
+    console.log('after sign up', client.auth.user());
+
+    return response.user;
+}
