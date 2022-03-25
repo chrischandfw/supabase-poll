9fca5d2e41036dbfc1a2a238b862ccec65a337a0
chris_chan <chrischandfw@gmail.com>
2022-03-24 22:45:53 -0500

    added event listeners to vote buttons and created display function for current question

--- polls/polls.js
+++ polls/polls.js
@@ -1,10 +1,16 @@
 const currentPollContainer = document.querySelector('.current-poll-container');
 const publishButton = document.querySelector('.publish-button');
 const pollFarmEl = document.querySelector('.poll-farm-list');
+
 const formEl = document.querySelector('form');
 const currentQuestEl = document.querySelector('.current-question');
 const currentOp1El = document.querySelector('.option-one');
 const currentOp2El = document.querySelector('.option-two');
+
+const addOption1Button = document.querySelector('.add-option-one');
+const subtractOption1Button = document.querySelector('.subtract-option-one');
+const addOption2Button = document.querySelector('.add-option-two');
+const subtractOption2Button = document.querySelector('.subtract-option-two'); 
 
 let currentQuestion = '';
 let option1 = '';
@@ -21,15 +27,35 @@ formEl.addEventListener('submit', (e) => {
     option1 = data.get('option-1');
     option2 = data.get('option-2');
 
-    currentQuestEl.textContent = currentQuestion;
-    currentOp1El.textContent = option1;
-    currentOp2El.textContent = option2;
+    displayCurrentQuestion();
 	
     formEl.reset();
 
 });
 
-function displayCurrentQuestion() {
-	
+addOption1Button.addEventListener('click', () => {
+    vote1++;
+    currentOp1El.textContent = `${option1} (${vote1})`;
 
+});
+
+addOption2Button.addEventListener('click', () => {
+    vote2++;
+    currentOp2El.textContent = `${option2} (${vote2})`;
+});
+
+subtractOption1Button.addEventListener('click', () => {
+    vote1--;
+    currentOp1El.textContent = `${option1} (${vote1})`;
+});
+
+subtractOption2Button.addEventListener('click', () => {
+    vote2--;
+    currentOp2El.textContent = `${option2} (${vote2})`;
+});
+
+function displayCurrentQuestion() {
+    currentQuestEl.textContent = currentQuestion;
+    currentOp1El.textContent = option1;
+    currentOp2El.textContent = option2;
 }
