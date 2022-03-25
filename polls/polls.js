736ed7d59424c037fc90a80ba97c01e5ca9d0adf
chris_chan <chrischandfw@gmail.com>
2022-03-24 22:13:35 -0500

    added text content to form Dom elements

--- polls/polls.js
+++ polls/polls.js
@@ -2,9 +2,11 @@ const currentPollContainer = document.querySelector('.current-poll-container');
 const publishButton = document.querySelector('.publish-button');
 const pollFarmEl = document.querySelector('.poll-farm-list');
 const formEl = document.querySelector('form');
+const currentQuestEl = document.querySelector('.current-question');
+const currentOp1El = document.querySelector('.option-one');
+const currentOp2El = document.querySelector('.option-two');
 
-
-let question = '';
+let currentQuestion = '';
 let option1 = '';
 let option2 = '';
 let vote1 = '';
@@ -15,8 +17,14 @@ formEl.addEventListener('submit', (e) => {
 
     const data = new FormData(formEl);
 
-    data.get('question');
-    data.get('option-1');
-    data.get('option-2');
+    currentQuestion = data.get('question');
+    option1 = data.get('option-1');
+    option2 = data.get('option-2');
+
+    currentQuestEl.textContent = currentQuestion;
+    currentOp1El.textContent = option1;
+    currentOp2El.textContent = option2;
 	
+    formEl.reset();
+
 });
