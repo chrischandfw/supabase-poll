5c404099ff65eea6609864169196f040809059b8
chris_chan <chrischandfw@gmail.com>
2022-03-24 23:15:02 -0500

    added 2 async functions in fetch utils

--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,28 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZ
 const SUPABASE_URL = 'https://wkkubdeijvrntdmunqer.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+//we need 2 async functions to create poll and get poll
+
+export async function createPoll() {
+    const response = await client
+        .from('poll_farm')
+        .insert({ 
+			//snake cases match supabase table (sql data)
+            question: 'someValue', 
+            option_1: 'someValue', 
+            option_2: 'someValue',
+            votes_1: 0,
+            votes_2: 0, 
+        });
+    return response.body;
+}
+
+
+export async function getPolls() {
+    const response = await client
+        .from('poll_farm')
+        .select('*');
+
+    return response.body;
+}
